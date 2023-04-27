2f3b54ae4c369af1d7a897e1a58b7a3146a34f92
juand <juandiegoimbachihoyos@gmail.com>
2023-04-27 18:57:43 -0500

    Añadi la tarea de los ejercicios de while y for 1

---
+++ Ciclos/for1coment.py
@@ -0,0 +1,18 @@
+for i in range(11):
+    print(i)
+#Indicamos que para la variable i imprima su contenido hasta el 11 -1, Simpre llega al limite menos 1 y si no se indica el inicio comenzara desde 0
+for j in range (1,11):
+    print(j)
+#Indicamos que para la variable i que imprima su contenido iniciando desde el numero 1 hasta el 11-1 por lo ya indicado
+for k in range(0,11,2):
+    print(k)
+#En este caso estamos indicando los valores de inicio(el primer numero) los valores de fin(el segundo numero) y los valores de aumento o decrecion(el tercer numero)
+for i in range(10,-1,-2):
+    print(i)
+#En este caso es una cuenta "regresiva" por ende se utilizan los negativos en el fin y el valor de decrecion
+for i in range(11):
+    print('Amo programar en python')
+#En este caso le asignamos una cadena de texto a las repeticiones de la variable i indicadas por el 11 
+for i in range(1,11):
+    print(i**2)
+#para este caso elevamos al cuadrado (potenciamos a la 2) el valor numerico de cada dato de la vaiable i
